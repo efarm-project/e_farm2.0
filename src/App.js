cb7d802b824308987a0693bf9745e8fdf28ffd51
CodeGurup <[ngugipeter892@gmail.com]>
2024-11-15 21:58:25 +0300

    jobmatching

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import LandingPage from './pages/landingPage';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import EdResource from './pages/educationalResource';
+import JobMatching from './pages/jobmatching';
+import  Registerpage from './pages/registerPage';
+import LoginPage from './pages/loginPage';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/education' element={<EdResource/>}/>
+        <Route path='/jobmatching' element={<JobMatching />} />
+        <Route path ='/register' element={< Registerpage />} />
+        <Route path ='/login' element={<LoginPage/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./styles/loginpage.css";
+
+const LoginPage = () => {
+    const [PhoneNumber, setPhonenumber] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="loginPage">
+            <div className="inputContainer">
+                <h2 className="title">Welcome Back</h2>
+                <p className="subtitle">Sign in to your account</p>
+
+                <label className="label" htmlFor="phonenumber">Phone number. </label>
+                <input
+                    type="phonenumber"
+                    id="Phone Number"
+                    className="inputField"
+                    placeholder="Phone number"
+                    value={PhoneNumber}
+                    onChange={(e) => setPhonenumber(e.target.value)}
+                />
+
+                <label className="label" htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    className="inputField"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button className="loginButton">Sign In</button>
+
+                <p className="forgotPassword">
+                    <a href="#" className="forgotPasswordLink">Forgot Password?</a>
+                </p>
+
+                <p className="registerText">
+                    Don't have an account? <a href="/register" className="registerLink">Sign Up</a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
